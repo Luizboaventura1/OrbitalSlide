e7614a5f53d999b6851fc61ca265d97da615a358
Luiz Henrique <luizboaventura24@gmail.com>
2025-03-16 20:51:16 -0300

    feature: add slider component

---
+++ src/interfaces/SliderOptions.ts
@@ -0,0 +1,10 @@
+export default interface SliderOptions {
+  loop?: boolean;       
+  autoplay?: boolean;      
+  autoplaySpeed?: number;  
+  draggable?: boolean;     
+  infinite?: boolean;     
+  speed?: number;        
+  navigation?: boolean;  
+  pagination?: boolean; 
+}
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-});
+  plugins: [],
+  test: {
+    environment: "happy-dom",
+  },
+})
